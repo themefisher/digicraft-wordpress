892959dee20a860c523f53d03004d7ecb9ce3a5f
Mehedi Sharif <mehedishariftitas@gmail.com>
2017-05-18 02:52:45 +0600

    Added TMG Plugin for plugin activation

--- functions.php
+++ functions.php
@@ -181,10 +181,17 @@ function pw_format_currency( $formatted, $currency, $price ) {
 add_filter( 'edd_usd_currency_filter_before', 'pw_format_currency', 10, 3 );
 
 
-
+//Codestar Fromwork Added
 if ( file_exists(  __DIR__ . '/libs/cs-framework/cs-framework.php' ) ) {
 	require_once  __DIR__ . '/libs/cs-framework/cs-framework.php';
 }
+
+//TMG Plugin for plugin activation
+if ( file_exists(  __DIR__ . '/libs/tmg/class-tgm-plugin-activation.php' ) ) {
+	require_once  __DIR__ . '/libs/tmg/class-tgm-plugin-activation.php';
+}
+
+
 
 //EDD Fucntions
 
@@ -205,3 +212,118 @@ function swift_purchase_link_text( $download_id ) {
 // rehook/add our function back to the same location as before
 add_action( 'edd_after_download_content', 'swift_purchase_link_text' );
 
+
+
+//TMG Plugin Settings
+add_action( 'tgmpa_register', 'tf_swift_register_required_plugins' );
+function tf_swift_register_required_plugins() {
+	/*
+	 * Array of plugin arrays. Required keys are name and slug.
+	 * If the source is NOT from the .org repo, then source is also required.
+	 */
+	$plugins = array(
+		array(
+			'name'      => 'Easy Digital Downloads',
+			'slug'      => 'easy-digital-downloads',
+			'required'  => true,
+		),
+		array(
+			'name'      => 'MailChimp for WordPress',
+			'slug'      => 'mailchimp-for-wp',
+			'required'  => true,
+		),
+
+
+
+	);
+
+	$config = array(
+		'id'           => 'tf-swift',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'default_path' => '',                      // Default absolute path to bundled plugins.
+		'menu'         => 'tgmpa-install-plugins', // Menu slug.
+		'has_notices'  => true,                    // Show admin notices or not.
+		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
+		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
+		'message'      => '',                      // Message to output right before the plugins table.
+
+		/*
+		'strings'      => array(
+			'page_title'                      => __( 'Install Required Plugins', 'tf-swift' ),
+			'menu_title'                      => __( 'Install Plugins', 'tf-swift' ),
+			/* translators: %s: plugin name. * /
+			'installing'                      => __( 'Installing Plugin: %s', 'tf-swift' ),
+			/* translators: %s: plugin name. * /
+			'updating'                        => __( 'Updating Plugin: %s', 'tf-swift' ),
+			'oops'                            => __( 'Something went wrong with the plugin API.', 'tf-swift' ),
+			'notice_can_install_required'     => _n_noop(
+				/* translators: 1: plugin name(s). * /
+				'This theme requires the following plugin: %1$s.',
+				'This theme requires the following plugins: %1$s.',
+				'tf-swift'
+			),
+			'notice_can_install_recommended'  => _n_noop(
+				/* translators: 1: plugin name(s). * /
+				'This theme recommends the following plugin: %1$s.',
+				'This theme recommends the following plugins: %1$s.',
+				'tf-swift'
+			),
+			'notice_ask_to_update'            => _n_noop(
+				/* translators: 1: plugin name(s). * /
+				'The following plugin needs to be updated to its latest version to ensure maximum compatibility with this theme: %1$s.',
+				'The following plugins need to be updated to their latest version to ensure maximum compatibility with this theme: %1$s.',
+				'tf-swift'
+			),
+			'notice_ask_to_update_maybe'      => _n_noop(
+				/* translators: 1: plugin name(s). * /
+				'There is an update available for: %1$s.',
+				'There are updates available for the following plugins: %1$s.',
+				'tf-swift'
+			),
+			'notice_can_activate_required'    => _n_noop(
+				/* translators: 1: plugin name(s). * /
+				'The following required plugin is currently inactive: %1$s.',
+				'The following required plugins are currently inactive: %1$s.',
+				'tf-swift'
+			),
+			'notice_can_activate_recommended' => _n_noop(
+				/* translators: 1: plugin name(s). * /
+				'The following recommended plugin is currently inactive: %1$s.',
+				'The following recommended plugins are currently inactive: %1$s.',
+				'tf-swift'
+			),
+			'install_link'                    => _n_noop(
+				'Begin installing plugin',
+				'Begin installing plugins',
+				'tf-swift'
+			),
+			'update_link' 					  => _n_noop(
+				'Begin updating plugin',
+				'Begin updating plugins',
+				'tf-swift'
+			),
+			'activate_link'                   => _n_noop(
+				'Begin activating plugin',
+				'Begin activating plugins',
+				'tf-swift'
+			),
+			'return'                          => __( 'Return to Required Plugins Installer', 'tf-swift' ),
+			'plugin_activated'                => __( 'Plugin activated successfully.', 'tf-swift' ),
+			'activated_successfully'          => __( 'The following plugin was activated successfully:', 'tf-swift' ),
+			/* translators: 1: plugin name. * /
+			'plugin_already_active'           => __( 'No action taken. Plugin %1$s was already active.', 'tf-swift' ),
+			/* translators: 1: plugin name. * /
+			'plugin_needs_higher_version'     => __( 'Plugin not activated. A higher version of %s is needed for this theme. Please update the plugin.', 'tf-swift' ),
+			/* translators: 1: dashboard link. * /
+			'complete'                        => __( 'All plugins installed and activated successfully. %1$s', 'tf-swift' ),
+			'dismiss'                         => __( 'Dismiss this notice', 'tf-swift' ),
+			'notice_cannot_install_activate'  => __( 'There are one or more required or recommended plugins to install, update or activate.', 'tf-swift' ),
+			'contact_admin'                   => __( 'Please contact the administrator of this site for help.', 'tf-swift' ),
+
+			'nag_type'                        => '', // Determines admin notice type - can only be one of the typical WP notice classes, such as 'updated', 'update-nag', 'notice-warning', 'notice-info' or 'error'. Some of which may not work as expected in older WP versions.
+		),
+		*/
+	);
+
+	tgmpa( $plugins, $config );
+}
